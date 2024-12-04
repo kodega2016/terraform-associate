8da73f2fddc7d0bf5a77f94bef5daf2ba9e677e2
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2024-12-05 05:13:47 +0545

    added multiple variables

--- input_variables/compute.tf
+++ input_variables/compute.tf
@@ -1,13 +1,12 @@
 resource "aws_instance" "webserver" {
   ami                         = "ami-0453ec754f44f9a4a"
-  instance_type               = "t3.micro"
-  associate_public_ip_address = false
+  instance_type               = var.ec2_instance_size
+  associate_public_ip_address = var.allow_public_ip_address
 
 
-  tags = {
-    Author  = "bikue"
-    Purpose = "Demo"
-  }
+  tags = merge(var.common_tags, {
+    Name = "webserver"
+  })
 
   root_block_device {
     delete_on_termination = true
--- input_variables/variables.tf
+++ input_variables/variables.tf
@@ -3,3 +3,25 @@ variable "aws_region" {
   description = "the default region for the aws resources"
   type        = string
 }
+
+variable "ec2_instance_size" {
+  default     = "t2.micro"
+  type        = string
+  description = "This is the default ec2 instance type"
+}
+
+variable "allow_public_ip_address" {
+  type        = bool
+  default     = false
+  description = "This is the flag to assign public ip address to ec2 instance,default is false"
+}
+variable "common_tags" {
+  type        = map(string)
+  description = "This is the common tags for all the resources"
+  default = {
+    "Author"      = "biku"
+    "Purpose"     = "Demo"
+    "is_from_iac" = true
+  }
+
+}
