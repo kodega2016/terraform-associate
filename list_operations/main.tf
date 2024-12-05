fca028d3ab5f747c00242f17a0f6829057e9ba93
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2024-12-06 04:47:25 +0545

    added another operation for list

--- list_operations/main.tf
+++ list_operations/main.tf
@@ -1,13 +1,15 @@
 locals {
   double_numbers = [for num in var.numbers_list : num * 2]
   even_numbers   = [for num in var.numbers_list : num % 2 == 0]
-  first_names=[for name in var.objects_list:name.firstname]
+  first_names    = [for name in var.objects_list : name.firstname]
+  full_names     = [for name in var.objects_list : "${name.firstname} ${name.lastname}"]
 }
 
 output "results" {
   value = {
     double_numbers = local.double_numbers
     even_numbers   = local.even_numbers
-    first_names=local.first_names
+    first_names    = local.first_names
+    full_names     = local.full_names
   }
 }
